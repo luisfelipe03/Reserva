b2ea96a731ba9463b9611b0d12acbdf9f8da5ced
Luis Felipe <llipeandrade34@gmail.com>
2024-01-11 18:53:29 -0300

    basic config

--- back_end/src/main/java/br/com/reserva/Startup.java
+++ back_end/src/main/java/br/com/reserva/Startup.java
@@ -1,13 +1,13 @@
-package br.com.back_end;
+package br.com.reserva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackEndApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackEndApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
